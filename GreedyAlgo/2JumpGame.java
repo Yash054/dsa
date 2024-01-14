adc0f643f1354c8d8fa8c86ab4b2525d85f13c1f
Yash054 <cyash54prayag@gmail.com>
2024-01-14 22:00:17 +0530

    7

--- GreedyAlgo/2JumpGame.java
+++ GreedyAlgo/2JumpGame.java
@@ -1,3 +1,32 @@
+//The easiest way to think about this problem is to ask are the elements with a 0 value avoidable? this is the algorithm that I constructed to answer this question.Starting from the second to last element in the array we continue to decrement towards the start of the array. Only stopping if we hit an element with a value of 0; in this case we evaluate if there exist an element somewhere at the start of the array which has a jump value large enough to jump over this 0 value element.
+
+public class Solution {
+    public boolean canJump(int[] nums) {
+       if(nums.length < 2) return true;
+       
+       for(int curr = nums.length-2; curr>=0;curr--){
+           if(nums[curr] == 0){
+               int neededJumps = 1;
+               while(neededJumps > nums[curr]){
+                   neededJumps++;
+                   curr--;
+                   if(curr < 0) return false;
+               }
+           }
+       }
+       return true;
+    }
+}
+
+
+
+
+
+
+
+
+
+
 // 1st - 3ms solution ,  2nd is 2ms solution
 
 // class Solution {
