2f70a83804df3f8d716050329941c7aa6d9448bc
Yash054 <cyash54prayag@gmail.com>
2024-01-14 21:51:04 +0530

    6

--- GreedyAlgo/2JumpGame.java
+++ GreedyAlgo/2JumpGame.java
@@ -1,3 +1,18 @@
+// 1st - 3ms solution ,  2nd is 2ms solution
+
+// class Solution {
+//     public boolean canJump(int[] nums) {
+//      int reachable = 0;
+//     for (int i=0; i<nums.length; ++i) {
+//         if (i > reachable) return false;
+//         reachable = reachable>i+nums[i]?reachable:i + nums[i];
+//     }
+//     return true;
+//     }
+// }
+
+
+
 class Solution {
     public boolean canJump(int[] nums) {
         int n = nums.length, k = 1, zi = -1;
