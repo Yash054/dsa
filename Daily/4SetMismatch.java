ecdc504200c5209961f21bba1fff3e72b6957470
Yash054 <cyash54prayag@gmail.com>
2024-01-23 21:21:06 +0530

    16

--- Daily/4SetMismatch.java
+++ Daily/4SetMismatch.java
@@ -7,6 +7,30 @@
 // Input: nums = [1,1]
 // Output: [1,2]
 
+///////// Solution With Count sort Aproach
+// class Solution {
+//     public int[] findErrorNums(int[] nums) {
+//         int n = nums.length;
+//         int[] v = new int[n + 1];
+//         int missing = 0, duplicate = 0;
+
+//         for (int i = 0; i < n; i++) {
+//             v[nums[i]]++;
+//         }
+
+//         for (int i = 1; i < v.length; i++) {
+//             if (v[i] == 2) {
+//                 duplicate = i;
+//             }
+//             if (v[i] == 0) {
+//                 missing = i;
+//             }
+//         }
+
+//         return new int[]{duplicate, missing};
+//     }
+// }
+
 
 public int[] findErrorNums(int[] nums) {
     int n = nums.length;
