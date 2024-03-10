51982f88bcd784b2920e9257a8a3dbf0d4e7b694
Yash054 <cyash54prayag@gmail.com>
2024-03-10 22:30:32 +0530

    18h

--- trees/8MaxPathSum(hard).java
+++ trees/8MaxPathSum(hard).java
@@ -21,12 +21,13 @@ class Solution {
     }
     public int maxpath(TreeNode curr)
     {
+        //
         if(curr==null)
             return 0;
-        int le = Math.max(maxpath(curr.left),0);
+        int le = Math.max(maxpath(curr.left),0);// to handle case if, left node is -1, then it is better to leave the path to calc max path .
         int ri = Math.max(maxpath(curr.right),0);
-        max = (int)Math.max(max,le+ri+curr.val);
-        return  curr.val + Math.max(le,ri);
+        max = (int)Math.max(max,le+ri+curr.val);// measure the path in direction -  ^ 
+        return  curr.val + Math.max(le,ri);// forward the max path line so eithr left or right
     
 
 
