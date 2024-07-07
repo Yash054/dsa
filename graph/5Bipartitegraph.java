9810bf5c72745b8143f9de39786ab24e93eb6ecf
Yash054 <cyash54prayag@gmail.com>
2024-07-07 11:20:25 +0530

    10e

---
+++ graph/5Bipartitegraph.java
@@ -0,0 +1,8 @@
+
+
+///A bipartite graph  -- a graph which can be colored with only 2 color with no adjacent nodes having the same color. 
+// a graph having even node length cycle and a graph with no cycle is a bipartite graph.
+// graph with odd node length is non bipatite.
+public class Bipartitegraph {
+    
+}
